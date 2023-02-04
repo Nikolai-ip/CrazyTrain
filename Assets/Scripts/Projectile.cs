aba5a085929aee8a8f05979744a8130032df3686
Илья <tempestasnatus@gmail.com>
2023-02-04 17:34:01 +0300

    added projectile logic

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _angle;
+    [SerializeField] private bool _spawnedByPlayer;
+    [SerializeField] private int _ricochetLimit;
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = new Vector2(_speed * Mathf.Cos(_angle), _speed * Mathf.Sin(_angle));
+        transform.Rotate(0, 0, _angle * 180 / Mathf.PI);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == Mathf.Log(LayerMask.GetMask("Ricochet"), 2))
+        {
+            if (_ricochetLimit-- == 0)
+            {
+                Destroy(gameObject);
+            }
+            UpdateAngle(-_angle);
+        }
+        else// if (collision.collider.TryGetComponent(out Damagable entity))
+        {
+            if (collision.gameObject.tag == "Enemy" && !_spawnedByPlayer || collision.gameObject.tag == "Player" && _spawnedByPlayer)
+            {
+                Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                _rb.velocity = new Vector2(_speed * Mathf.Cos(_angle), _speed * Mathf.Sin(_angle));
+                return;
+            }
+            Destroy(gameObject);
+            //entity.TakeDamage();
+        } 
+    }
+
+    private void UpdateAngle(float newAngle)
+    {
+        _angle = newAngle;
+        _rb.velocity = new Vector2(_speed * Mathf.Cos(_angle), _speed * Mathf.Sin(_angle));
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        transform.Rotate(0, 0, _angle * 180 / Mathf.PI);
+    }
+}
