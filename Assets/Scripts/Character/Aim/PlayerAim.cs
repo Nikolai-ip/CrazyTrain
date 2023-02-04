5753fb4dd376605d69e387672165648c57f6a710
unknown <petruchok02@mail.ru>
2023-02-04 18:07:27 +0300

    Modify bullet

--- Assets/Scripts/Character/Aim/PlayerAim.cs
+++ Assets/Scripts/Character/Aim/PlayerAim.cs
@@ -1,42 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerAim : MonoBehaviour
 {
-    private bool isForwardRotate;
-    private Transform aimTransform;
+    private bool _isForwardRotate;
+    private Transform _aimTransform;
+
     private void Awake()
     {
-        isForwardRotate = true;
-        aimTransform = transform.Find("Aim");
+        _isForwardRotate = true;
+        _aimTransform = transform.Find("Aim");
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (mousePosition - transform.position).normalized;
-
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-
-       
-        Debug.Log(angle);
-
-        if (isForwardRotate)
+        if (_isForwardRotate)
         {
             if (Mathf.Abs(angle) > 90)
             {
                 MirrorPlayer();
-                isForwardRotate = false;
+                _isForwardRotate = false;
                 angle = -1 * angle;
             }
         }
@@ -45,20 +31,16 @@ public class PlayerAim : MonoBehaviour
             if (Mathf.Abs(angle) < 90)
             {
                 MirrorPlayer();
-                isForwardRotate = true;
+                _isForwardRotate = true;
                 angle = -1 * angle;
             }
         }
 
-        aimTransform.eulerAngles = new Vector3(0, 0, angle);
-
-
+        _aimTransform.eulerAngles = new Vector3(0, 0, angle);
     }
 
-    void MirrorPlayer()
+    private void MirrorPlayer()
     {
         transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
-        //aimTransform.localScale = new Vector2(-1 * aimTransform.localScale.x, aimTransform.localScale.y);
-        //transform.localScale = -1 * transform.localScale;
     }
 }
