d2ab6412640726e633280c73b39c474d222d7be5
unknown <petruchok02@mail.ru>
2023-02-04 18:14:55 +0300

    Add Keyboard Controller

---
+++ Assets/Scripts/Character/KeyboardInputController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class KeyboardInputController : MonoBehaviour
+{
+    [SerializeField] private MouseButton _shootKeyCode;
+    [SerializeField] private KeyCode _hideKeyCode;
+
+    void Update()
+    {
+        
+    }
+}
