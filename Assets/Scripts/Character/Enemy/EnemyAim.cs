74e67a658a4054bfeff3a27d6b2229b116b168f4
qwonfery <nikitavish@list.ru>
2023-02-05 22:03:16 +0300

    Revert "Merge branch 'EnemyAI'"
    This reverts commit c994b92372238a691964fdec868725f70bb849bc, reversing
    changes made to 07f124996c6d05c2bc022f3258be2e77242fc49f.

--- Assets/Scripts/Character/Enemy/EnemyAim.cs
+++ Assets/Scripts/Character/Enemy/EnemyAim.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class EnemyAim : Aim
 {
-    private GameObject playerObj = null;
+    [SerializeField] private GameObject playerObj = null;
     private Transform _playerTransform;
     private Vector3 _playerPosition;
     protected override void InitPosition()
     {
-        playerObj = GameObject.FindGameObjectWithTag("Player");
         _playerTransform = playerObj.transform;
         _playerPosition = _playerTransform.position;
         //if (playerObj is null) return;
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Character.Player;
 using UnityEngine;
@@ -7,31 +8,28 @@ public abstract class Weapon : MonoBehaviour, Shootable
     [SerializeField] protected float damage;
     [SerializeField] protected GameObject bulletPrefab;
     [SerializeField] protected Transform shootPoint;
-    [SerializeField] protected Aim characterAim;
+    [SerializeField] protected PlayerAim playerAim;
     [SerializeField] protected int bulletAmount;
+    [SerializeField] protected int currentBulletAmount;
     [SerializeField] protected int shootTimeDelayMs;
     [SerializeField] protected bool canShoot = true;
+    protected bool isReloading = true;
     protected Animator animator;
     [SerializeField] protected AudioSource gunshot;
-
-    private void Awake()
+    public event Action<bool, int> onReloadButtonIsPressed;
+    public virtual void Shoot()
     {
-
-        characterAim = GetComponentInParent<Aim>();
-        Debug.Log(characterAim.gameObject.name);
-    }
-    public void Shoot()
-    {
-        if (bulletPrefab != null && canShoot)
+        if (bulletPrefab != null && canShoot && currentBulletAmount>0)
         {
             var bullet = Instantiate(bulletPrefab);
             bullet.transform.position = shootPoint.position;
             gunshot.Play();
             if (bullet.TryGetComponent(out Projectile projectile))
             {
-                projectile.Angle = characterAim.AngleRad;
+                projectile.Angle = playerAim.AngleRad;
                 animator.SetTrigger("Shoot");
                 DelayBetweenShoot();
+                currentBulletAmount--;
             }
         }
     }
@@ -41,5 +39,21 @@ public abstract class Weapon : MonoBehaviour, Shootable
         canShoot = false;
         await Task.Delay(shootTimeDelayMs);
         canShoot = true;
+    }
+    public virtual void Reload()
+    {
+        onReloadButtonIsPressed?.Invoke(isReloading,currentBulletAmount);
+        if (!isReloading)
+        {
+            isReloading = true;
+        }
+        else
+        {
+            isReloading = false;
+        }
+    }
+    public virtual void PushBullet()
+    {
+        
     }
 }
