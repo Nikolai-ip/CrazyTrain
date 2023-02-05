3b7be445f422b04e93d8f83726272818ea195665
Ilya <tempestasnatus@gmail.com>
2023-02-05 18:31:34 +0300

    added health bar

--- Assets/Scripts/Character/Player/Player.cs
+++ Assets/Scripts/Character/Player/Player.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Character.Player
 {
-    public class Player : Entity
+    public class Player : MonoBehaviour, Damagable
     {
         [SerializeField] private Weapon _currentWeapon;
+        [SerializeField] private delegate void OnHealthChangedDelegate();
+        [SerializeField] private OnHealthChangedDelegate onHealthChangedCallback;
 
         public Weapon CurrentWeapon { private get => _currentWeapon; set => _currentWeapon = value; }
 
@@ -16,6 +19,20 @@ namespace Character.Player
         public void ShootCurrentWeapon()
         {
             CurrentWeapon.Shoot();
+        }
+
+        public void TakeDamage()
+        {
+            PlayerStats.Instance.TakeDamage(1);
+            if (PlayerStats.Instance.Health == 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        public void Heal()
+        {
+            PlayerStats.Instance.Heal(1);
         }
     }
 }
